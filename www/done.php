07c1fc5b4d0ccc6a8abf13d3ec1da5dabc0823d4
David Ziegler <drziegler@users.noreply.github.com>
2018-05-16 22:25:56 -0400

    New confirmation page
    Never implemented in 2018.

---
+++ www/done.php
@@ -0,0 +1,19 @@
+<?php
+include('vbsUtils.inc');
+?>
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+<link href="css/vbs.css" rel="stylesheet" type="text/css">
+<link href="css/layout.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div class="center"><h1>THANKS FOR REGISTERING</h1></div>
+	<div id="buttonSubGroup" style="position:fixed; width:100%; top:0;">
+		<a href="<?php echo HOME_PAGE?>"><input type="submit" class="button" name="submit" value="<?php echo HOME_BUTTON?>"></a>
+	</div>
+	<div style="position:fixed; width:100%; bottom:0"><h1>See you in July</h1></div>
+</body>
+</html>
