b9c5ff9aea4233a46adb8f1e9d461eeb8caf15b0
David Ziegler <drziegler@users.noreply.github.com>
2018-05-16 22:24:40 -0400

    2018 Enhancements
    Supports the new "C" type registration status.

--- www/register.php
+++ www/register.php
@@ -5,6 +5,11 @@ include_once('vbsUtils.inc');
 
 $confoNo = mt_rand();
 $_SESSION['confoNo'] = $confoNo;
+
+$sqlQuery = "SELECT count(*) as registered from students where registered = 'C'";
+$result = mysqli_query($vbsDBi, $sqlQuery);
+$registered = mysqli_fetch_assoc($result);
+mysqli_free_result($result);
 ?>
 <!doctype html>
 <!--[if lt IE 7]> <html class="ie6 oldie"> <![endif]-->
@@ -19,7 +24,7 @@ $_SESSION['confoNo'] = $confoNo;
 <title>VBS Registration</title>
 <link href="css/boilerplate.css" rel="stylesheet" type="text/css">
 <link href="css/layout.css" rel="stylesheet" type="text/css">
-<link href="css/textural.css" rel="stylesheet" type="text/css">
+<!--<link href="css/textural.css" rel="stylesheet" type="text/css"> -->
 <!--[if lt IE 9]>
 <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
@@ -28,15 +33,17 @@ $_SESSION['confoNo'] = $confoNo;
 <body><!--  version 1 -->
 <div id="Register" class="gridContainer">
 <div><h1>Hope Lutheran Church</h1></div>
-<div><h2>VBS-Registration</h2></div>
+<table style="border-style: none;"><tr><td width="10%"><a href="/index.php"><input type="submit" class="button" name="submit" value="<?php echo HOME_BUTTON?>"></a></td>
+	<td width="80%" align="center" class="h2">VBS-Registration</td><td width="10%">&nbsp;</td></tr></table>
 <div>
-    <a href="search.php"><div id="mnuBtn" class="find"><span>Find</span></div></a>
-    <a href="family.php"><div id="mnuBtn" class="family"><span>Family</span></div></a>
+	<a href="search.php"><div id="mnuBtn" class="find"><span>Find</span></div></a>
+    <a href="<?php echo FAMILY_PAGE ?>"><div id="mnuBtn" class="family"><span>Family</span></div></a>
     <a href="student.php"><div id="mnuBtn" class="student"><span>Students</span></div></a>
     <a href="staff.php"><div id="mnuBtn" class="staff"><span>Volunteers</span></div></a>
-</div>  
-<div id="mnuButton"><a href="confirm.php">Validate and Confirm</a></div>
-<div id="Footer">&copy; 2017 David R Ziegler &amp; Hope Lutheran Church</div>
+</div>
+<div id="counter">Over&nbsp;<?php echo $registered['registered']?>&nbsp;students&nbsp;shipwrecked</div>
+<!-- FOR TEST ENV ONLY <div id="counter">Over 20,000 Shipwrecked!</div> -->
+<div id="Footer">&copy; <?php echo date("Y");?> David R Ziegler &amp; Hope Lutheran Church</div>
 </div>
 </body>
 </html>
