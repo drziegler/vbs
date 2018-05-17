3b32d4d4e16d3b7ce7b9a4ee18edfcba8a7200d0
David Ziegler <drziegler@users.noreply.github.com>
2018-05-16 22:15:25 -0400

    Enhancements for 2018

--- www/family.php
+++ www/family.php
@@ -2,6 +2,9 @@
 session_start();
 include('vbsUtils.inc');
 require_once('Connections/vbsDB.php');
+$errMsgText="";
+
+if (DEBUG) print "_SESSION[familyID] = " . $_SESSION['family_id'] . "<br>";
 
 if (isset($_REQUEST['submit']) and $_REQUEST['submit']=="Save"){
 	/* skip this redirect test, else */
@@ -14,7 +17,13 @@ else{
 }
 
 function validate($form){
-	if (DEBUG) print "Line: " . __LINE__ . "-Validate<br>";
+	if (DEBUG) {
+		print "Line: " . __LINE__ . "-Validate<br>";
+		print "Line " . __LINE__ . "- _POST "; 
+		print_r($_POST);
+		print "<br>";
+	}
+	
 
 	$error = false;
 	global $errMsgText;
@@ -22,8 +31,8 @@ function validate($form){
 
 	/* Mandatory form elements */	
 	$mustExist  = array('prehelp'=>'Prehelp');
-	$notBlank   = array('first_name'=>'Family Name', 'address'=>'Address', 'zipcode'=>'Zip code');
-	$selectLst  = array('lsthomechurch'=>'Home Church');
+	$notBlank   = array('family_name'=>'Family Name', 'address'=>'Address', 'zipcode'=>'Zip code', 'email'=>'Email', 'lsthomechurch'=>'Home Church');
+
 	
 	/* Remove leading and trailing spaces */
 	foreach ($form as $key => $value){
@@ -31,11 +40,16 @@ function validate($form){
 	}
 	/* Assign trim changes back to the $_POST array */
 	$_POST = $form;
-
+	if (DEBUG) {
+		print "Line " . __LINE__ . "- _POST "; 
+		print_r($_POST);
+		print "<br>";
+	}
+	
 	/* Check for blank elements */
 	$blanks = array_intersect_key($form, $notBlank);
 	foreach ($blanks as $key => $value){
-		if (strlen($value)===0){
+		if (strlen(trim($value))===0){
 			$errMsg .= $notBlank[$key] . ",";
 			$error = true;
 		}
@@ -49,15 +63,23 @@ function validate($form){
 	}
 	/* If the element is missing, add a blank one to the array to avoid display errors */
 	$_POST = array_merge($form, $missing);	
-
-	/* Check for options not selected */
-	$selected = array_intersect_assoc($selectLst, $form);
-	foreach ($selected as $element){
-		$errMsg .= $element . ",";
+	if (DEBUG) {
+		print "Line " .__LINE__ . "-"; 
+		print_r($_POST);
+		print "<br>";
 	}
 
+	/* Check for options not selected */
+	$option = (isset($form['lstHomeChurch'])) ? $form['lstHomeChurch'] : false;
+	if (!$option) {
+		if (DEBUG) print "Line " . __LINE__ . "-Home church missing<br>";
+		$errMsg .= "Home Church,";
+		$error = true;
+	}
+
+
 	/* This assigns the error text to a variable outside the function */
-	$errMsgText = "Invalid items: " . trim($errMsg, ",");
+	$errMsgText = (strlen($errMsg)>0 ? "Need: " . trim($errMsg, ",") : "");
 	return !$error;
 }
 
@@ -74,16 +96,20 @@ if (empty($_POST['submit'])) $_POST['submit']='Display';
 if (DEBUG) print "Line: " . __LINE__ . "<br>";
 
 switch ($_POST['submit']) {
-	case 'Back' :
-		if (DEBUG) print "Line: " . __LINE__ . "-Back <br>";
+	case HOME_BUTTON :
 		header("Location: " . HOME_PAGE);
+		break;
+	case PREVIOUS_BUTTON :
+		header("Location: " . SEARCH_PAGE);
 		break;
 	case 'New' :
 		if (DEBUG) print "Line: " . __LINE__ . "-New<br>";
-		$rsFam = array('family_name'=>'','email'=>'','address'=>'','zipcode'=>'','prehelp'=>'','home_church'=>'','comments'=>'','family_id'=>'');
+		$rsFam = array('family_name'=>'','email'=>'','address'=>'','zipcode'=>'','prehelp'=>'','home_church'=>'','comments'=>'','family_id'=>'',								'city'=>'','state'=>'');
+		$city = $state = '';
 		$_SESSION['family_id']='0';
+		$_SESSION['family_name'] = '';
 		break;
-	case 'Next' :
+	case NEXT_PAGE :
 		if (DEBUG) print "Line: " . __LINE__ . "-Next<br>";
 	case 'Save' :
 		if (validate($_POST)){
@@ -100,7 +126,7 @@ switch ($_POST['submit']) {
 				mysqli_real_escape_string($vbsDBi, $_POST['zipcode']),
 				mysqli_real_escape_string($vbsDBi, $_POST['lstHomeChurch']),
 				mysqli_real_escape_string($vbsDBi, $_POST['prehelp']),
-				mysqli_real_escape_string($vbsDBi, $_POST['comment']),
+				mysqli_real_escape_string($vbsDBi, $_POST['comments']),
 				$_SESSION['confoNo']
 				);
 					
@@ -109,6 +135,7 @@ switch ($_POST['submit']) {
 					if (DEBUG) print "Line " . __LINE__ . "<br>";	
 					$_SESSION['family_id'] = mysqli_insert_id($vbsDBi);
 					$rsFamily['family_id'] = $_SESSION['family_id'];
+					$_SESSION['family_name'] = $_POST['family_name'];
 					insertStats($vbsDBi, $_SESSION['family_id'], 'new');
 					writeLog("Inserted new family id " . $_SESSION['family_id'] . " for " . $_POST['family_name']);
 					
@@ -147,7 +174,7 @@ switch ($_POST['submit']) {
 					mysqli_real_escape_string($vbsDBi, trim($_POST['zipcode'])),
 					mysqli_real_escape_string($vbsDBi, trim($_POST['lstHomeChurch'])),
 					mysqli_real_escape_string($vbsDBi, trim($_POST['prehelp'])),
-					mysqli_real_escape_string($vbsDBi, trim($_POST['comment'])),
+					mysqli_real_escape_string($vbsDBi, trim($_POST['comments'])),
 					mysqli_real_escape_string($vbsDBi, $_POST['family_id']));
 
 				if (!mysqli_query($vbsDBi, $sqlStmt)){
@@ -158,17 +185,31 @@ switch ($_POST['submit']) {
 				else {
 					if (DEBUG) print "Line: " . __LINE__ . "-Updated:family<br>";
 					writeLog("Updated family id as " . $sqlStmt);
+					$_SESSION['family_name'] = $_POST['family_name'];
 				}
 			}
-			if ($_POST['submit']=='Next'){
-				header("Location: contacts.php");
+			if ($_POST['submit']==NEXT_PAGE){
+				header("Location: " . CONTACT_PAGE);
 			}
 		}
 		else {
-			/* We have an error condition, display the error message */
-			if (DEBUG) print "Line: " . __LINE__ . "-Failed Validation<br>";
-			$errMsgText = "Please correct missing data and save again.";
+			/* Validation failed. Display the error message */
+			//@@$errMsgText = "Please correct missing data and save again.";
+			//todo: Fix _POST back to rsFAM.  field names are not the same, e.g. home_church vs lsthomechurch.
 			$rsFam = $_POST;
+			$rsFam['home_church'] = $rsFam['lstHomeChurch'];
+			$city = $_POST['city'];
+			$state = $_POST['state'];
+
+			if (DEBUG) {
+				print "Line: " . __LINE__ . "-Failed Validation<br>" . "Line " . __LINE__ . "- _POST "; 
+				print_r($_POST);
+				print "<br>" . "Line " . __LINE__ . "- rsFam "; 
+				if (isset($rsFam)) print_r($rsFam);
+				print "<br>";
+			}
+
+			
 			break;
 		}
 	case "Display" :
@@ -208,10 +249,10 @@ switch ($_POST['submit']) {
 }  /* End of switch statement */
 
 
-$query_rsChurchList = "SELECT HOME_CHURCH FROM churches ORDER BY DISP_ORDER";
+$query_rsChurchList = "SELECT HOME_CHURCH FROM churches ORDER BY DISP_ORDER, HOME_CHURCH";
 $rsChurchResult = mysqli_query($vbsDBi, $query_rsChurchList);
 $rsChurchList = mysqli_fetch_assoc($rsChurchResult);
-
+if (DEBUG) print (mysqli_num_rows($rsChurchResult) . " Church rows fetched.");
 
 ?>
 <!doctype html>
@@ -227,7 +268,6 @@ $rsChurchList = mysqli_fetch_assoc($rsChurchResult);
 <title>VBS - Family</title>
 <link href="css/boilerplate.css" rel="stylesheet" type="text/css">
 <link href="css/layout.css" rel="stylesheet" type="text/css">
-<link href="css/textural.css" rel="stylesheet" type="text/css">
 <!--[if lt IE 9]>
 <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 <![endif]-->
@@ -237,33 +277,41 @@ $rsChurchList = mysqli_fetch_assoc($rsChurchResult);
 <body>
 <div id="Family" class="gridContainer">
 <div><h1>VBS - Family</h1></div>
-<div><h2>Edit your family information then click next.</h2></div>
+<?php if (strlen($errMsgText)) { ?>
+	<div><h2 class="error"><?php echo $errMsgText?></h2></div>
+<?php } else { ?>
+	<div><h2>Edit your family information then click next.</h2></div>
+<?php } ?>
 <div id="dataLayout">
 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>" method="post" name="frmFamily" target="_self">
 <table cellspace="0">
-	<tr><td class="label">*&nbsp;Family Name</td><td class="value"><input name="family_name" type="text" value="<?php echo $rsFam['family_name'];?>"></td></tr>
-	<tr><td class="label">*&nbsp;Address</td><td class="value"><input name="address" type="text" value="<?php echo $rsFam['address'];?>"></td></tr>
+	<tr><td class="label">*&nbsp;<span class="popup" onclick="myPopUp('hFamName')">Family Name<span class="popuptext" id="hFamName">Enter your family name in the format you want it to appear on correspondence to you, e.g. Mr &amp; Mrs John Doe.</span></span></td><td class="value"><input name="family_name" type="text" value="<?php echo $rsFam['family_name'];?>"></td></tr>
+	<tr><td class="label">*&nbsp;<span class="popup" onclick="myPopUp('hAddress')">Address<span class="popuptext" id="hAddress">Enter your street address or mailing address.</span></span></td><td class="value"><input name="address" type="text" value="<?php echo $rsFam['address'];?>"></td></tr>
 	<tr><td class="label">*&nbsp;<span class="popup" onclick="myPopUp('hZip')">Zipcode<span class="popuptext" id="hZip">Enter your 5-digit zipcode.  We'll look up the city and state.</span></span></td><td class="value"><input name="zipcode" type="number" min="0" max="99999" value="<?php echo $rsFam['zipcode'];?>" ></td></tr>
-	<?php if (intval($totalRows_rsCityState)>0){ ?>
-    <tr><td class="label"><span class="popup" onclick="myPopUp('hCity')">City, State<span class="popuptext" id="hCity">You can't enter anything here.  We will calculate your city and state from your zipcode. Eh?  You're from Canada?  Call us to make sure you can still enter our country!"</span></span></td><td class="value"><span><?php echo (intval($totalRows_rsCityState)>0) ? $city . ', ' . $state : ""; ?></span></td></tr>
-    <?php } ?>
-    <tr><td class="label">*&nbsp;Email</td><td class="value"><input name="email" type="email" value="<?php echo $rsFam['email'];?>"></td></tr>
+	<!--@@ <?php //if (intval($totalRows_rsCityState)>0){ ?> -->
+    <tr><td class="label"><span class="popup" onclick="myPopUp('hCity')">City, State<span class="popuptext" id="hCity">You can't enter anything here.  We will calculate your city and state from your zipcode. Eh?  You're from Canada?  Call us to make sure you can still enter our country!</span></span></td><td class="value"><span><?php echo (strlen($city)>0 || strlen($state)>0) ? $city . ', ' . $state : ""; ?></span></td></tr>
+    <!-- @@ <?php //} ?> -->
+    <tr><td class="label">*&nbsp;<span class="popup" onclick="myPopUp('hEmail')">Email<span class="popuptext" id="hEmail">Enter the email address to use for vbs correspondence.</span></span></td><td class="value"><input name="email" type="email" value="<?php echo $rsFam['email'];?>"></td></tr>
     <tr><td class="label">*&nbsp;<span class="popup" onclick="myPopUp('hChurch')">Home Church<span class="popuptext" id="hChurch">Select your home church from the drop down list.  If not listed, select other and enter your home church into the comments box.</span></span></td><td class="value"><select name="lstHomeChurch" style="width:90%;">
     <option value="">Select Home Church</option>
 	<?php do { ?>
-    <option value="<?php echo $rsChurchList['HOME_CHURCH'];?>"<?php if (!(strcmp($rsChurchList['HOME_CHURCH'], $rsFam['home_church']))) {echo "selected=\"selected\"";} ?>><?php echo $rsChurchList['HOME_CHURCH']?></option>
+<?php //@@REMOVE AFTER DEBUGGING
+	 	print "Line " . __LINE__ . " rsFam = ";
+	    print_r($rsFam); ?>
+	<option value="<?php echo $rsChurchList['HOME_CHURCH'];?>"<?php if (!(strcmp($rsChurchList['HOME_CHURCH'], $rsFam['home_church']))) {echo "selected=\"selected\"";} ?>><?php echo $rsChurchList['HOME_CHURCH']?></option>
     <?php } while ($rsChurchList = mysqli_fetch_assoc($rsChurchResult)); ?>
     </select></td></tr>
 	<tr><td class="label">*&nbsp;<span class="popup" onclick="myPopUp('hPrepH')">Prep help<span class="popuptext" id="hPrepH">If you are able to help prepare for VBS with tasks that can be done from home, check the yes box.  We'll contact you.  We guarantee it.</span></span></td><td class="value"><input <?php if (!(strcmp($rsFam['prehelp'],"Y"))) {echo "checked=\"checked\"";} ?> name="prehelp" type="radio" value="Y">&nbsp;Yes <input <?php if (!(strcmp($rsFam['prehelp'],"N"))) {echo "checked=\"checked\"";} ?> name="prehelp" type="radio" value="N">&nbsp;No</td></tr>
-	<tr><td class="label">Comments:</td><td class="value"><textarea name="comment" cols="" rows="3" style="width:90%;"><?php echo $rsFam['comments']; ?></textarea></td></tr>
-    <tr><td>* required  <span class="popup" onclick="myPopUp('help')">Help available<span class="popuptext" id="help">Use this form to update family information.  When done, click NEXT to enter phone information. Click the underlined labels for detailed popup help. Click again to close it.</span></span></td><td><input type="submit" name="submit" value="Save"></td></tr>
+	<tr><td class="label">Comments:</td><td class="value"><textarea name="comments" cols="" rows="3" style="width:90%;"><?php echo $rsFam['comments']; ?></textarea></td></tr>
+    <tr><td>* required  <span class="popup" onclick="myPopUp('help')">Help available<span class="popuptext" id="help">Use this form to update family information.  When done, click 'Next' to continue. Click the underlined labels for detailed popup help. Click again to close it.</span></span></td><td><input type="submit" name="submit" value="Save"></td></tr>
 </table>
 	<input type="hidden" name="family_id" value="<?php echo $rsFam['family_id'];?>">
     <input type="hidden" name="city" value="<?php echo $city;?>">
     <input type="hidden" name="state" value="<?php echo $state;?>">
     <div id="buttonGroup" class="center">
-    	<input type="submit" name="submit" class="button" value="Back">
-        <input type="submit" name="submit" class="button" value="Next">
+		<input type="submit" name="submit" class="button" value="<?php echo HOME_BUTTON?>">
+    	<input type="submit" name="submit" class="button" value="<?php echo PREVIOUS_BUTTON?>">
+        <input type="submit" name="submit" class="button" value="<?php echo NEXT_PAGE?>">
     </div>
     </form>
 </div>
