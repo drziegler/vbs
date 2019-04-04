cfe3c5f7113161c37af2667b37ba5be8db3c1019
David Ziegler <drziegler@users.noreply.github.com>
2019-04-03 22:44:08 -0400

    First Clearances page
    New in 2019, a clearance information page will display for families that have registered volunteers over the age of 18.

---
+++ www/clearances.php
@@ -0,0 +1,30 @@
+<?php 
+include("vbsUtils.inc");
+?>
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Frameset//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>VBS Volunteer Clearances</title>
+<link rel="stylesheet" media="screen and (min-device-width: 320px) and (orientation: portrait)" href="css/mobile.css" > 
+<link rel="stylesheet" media="screen and (max-device-width: 640px) and (orientation: landscape)" href="css/mobile.css" >
+<link rel="stylesheet" media="screen and (min-device-width: 768px) and (orientation: landscape)" href="css/tablet.css" >
+<link rel="stylesheet" media="screen and (min-device-width: 481px) and (orientation: portrait)" href="css/tablet.css" >
+<!-- <link rel="stylesheet" media="screen and (min-width: 1224px)" href="css/desktop.css" > -->
+</head>
+<body>
+<div class="gridContainer">
+	<h1>X VBS Volunteer Clearances Information</h1>
+	<p>VBS Volunteers that are 18 years of age and older, including parents in the <strong>"Mommy & Me Class"</strong>, are required to have a current PA Criminal Record Check and Child Abuse Clearance, and sign a Volunteer Disclosure Statement. Also, anyone that has not lived in the state of Pennsylvania consecutively over the past 10 years will need a Federal Criminal History Check (FBI Fingerprinting).</p> 
+	<p>If you have these and they are less than 5 years (60 months) old, you can turn in copies to Jennifer in the church office.  Please put them in an envelope labeled: for Julie Sekol or mail them to Julie Sekol c/o Hope Lutheran Church, P O  Box 1030, Cherryville PA 18035-1030.  You can print a copy of the <a href="http://keepkidssafe.pa.gov/cs/groups/webcontent/documents/document/c_160267.pdf" target="_new">Volunteer Disclosure Statement</a> or obtain a paper copy from the church office.</p>   
+	<p>If you do not have current copies of your clearances, follow <a href="http://keepkidssafe.pa.gov/resources/clearances/index.htm" target="_new">these directions</a> or email our <a href="mailto:clearances@hopecherryville.org">Clearance Coordinator</a>, Julie Sekol or call 610-984-3625 and she will be happy to help you. Please note that the PA Child Abuse Clearance and the PA Criminal Record Check are free for volunteers; however there is a fee for the fingerprints which are needed for any volunteers that lived outside the state of PA at any point in the past 10 years.  Also, it can take a few weeks for you to receive the Child Abuse Clearance and FBI Fingerprint results, so please do not delay in completing these as we must have them prior to the start of Vacation Bible School. Thank you!</p>
+    <div id="buttonGroup" class="center">
+		<form method="post" name="frmClearances" target="_self" action="<?php echo htmlspecialchars(HOME_PAGE);?>" style="display:inline-block">
+			<input type="submit" name="submit" class="button center" value="<?php echo HOME_BUTTON?>">&nbsp;
+		</form>
+    </div>
+</div>
+</body>
+</html>
