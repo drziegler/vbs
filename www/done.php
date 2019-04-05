6602bd061938cbb9a1ed8d648e36e5afcb7d3eb8
David Ziegler <drziegler@users.noreply.github.com>
2019-04-05 15:57:41 -0400

    Enhanced to use three css files
    Promotes better separation of media styles in different physical files.

--- www/done.php
+++ www/done.php
@@ -6,15 +6,19 @@ include('vbsUtils.inc');
 <head>
 <meta charset="utf-8">
 <title>Untitled Document</title>
-<link href="css/vbs.css" rel="stylesheet" type="text/css">
-<link href="css/layout.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" media="screen and (min-width: 320px) and (max-width: 480px)" href="css/mobile.css" >
+<link rel="stylesheet" media="screen and (min-width: 768px) and (max-width: 1024px)" href="css/tablet.css" >
+<link rel="stylesheet" media="screen and (min-width: 1224px)" href="css/desktop.css" >
+
 </head>
 <body>
-	<div class="center"><h1>THANKS FOR REGISTERING</h1></div>
+<div class="gridContainer">
+	<div class="center"><h1>T THANKS FOR REGISTERING</h1></div>
 	<div id="buttonSubGroup" style="position:fixed; width:100%; top:0;">
 		<a href="<?php echo HOME_PAGE?>"><input type="submit" class="button" name="submit" value="<?php echo HOME_BUTTON?>"></a>
 	</div>
-	<center><img src="images/vbs-logo.jpg"></center>
-	<div style="position:fixed; width:100%; bottom:0"><h1>See you in July</h1></div>
+	<div id="done-logo" class="vertical-center"><center><img src="images/vbs-logo.jpg"></center></div>
+	<div style="position:fixed; width:99%; bottom:0"><h1 class="center">See you in July</h1></div>
+</div>
 </body>
 </html>
