64a6f17178c0b2c30b5fd5d91430a6d08ae40156
David Ziegler <drziegler@users.noreply.github.com>
2019-04-12 11:35:04 -0400

    Clean up layout
    Changed Age Group to Clearance Required to better reflect use of field.  Centered booleans (requires new layout.css)

--- www/confirm.php
+++ www/confirm.php
@@ -1,4 +1,4 @@
-_<?php
+<?php
 /* This function formats the email to the vbs mail box. */
 /* Errors in this function as in the 2000 range */
 session_start();
@@ -6,6 +6,7 @@ include_once('vbsUtils.inc');
 require_once('Connections/vbsDB.php');
 define("SUBMIT_REGISTRATION", "Submit Registration");
 define("CANCEL_REGISTRATION", "Cancel");
+define("FILE_NAME", '[CONFIRM] ');
 $family = array();
 $studentTotal = 0;
 $staffTotal = 0;
@@ -24,14 +25,14 @@ function confirm($famID){
 
 
 	$sql="update students set registered='C' where registered='Y' and family_id=$famID";
-	writeLog($sql);
+	writeLog(FILE_NAME . __LINE__ . $sql);
 	if (!mysqli_query($vbsDBi, $sql)){
 		writeErr("Unable to confirm students for famID $famID.", __FUNCTION__, __LINE__, 2001);
 		$studentsConfirmed=false;
 	}
 
 	$sql="update staff set registered='C' where registered='Y' and family_id=$famID";
-	writeLog($sql);
+	writeLog(FILE_NAME . __LINE__ . $sql);
 	if (!mysqli_query($vbsDBi, $sql)){
 		writeErr("Unable to confirm staff for famID $famID.", __FUNCTION__, __LINE__, 2002);
 		$staffConfirmed=false;
@@ -65,25 +66,20 @@ function sendVBSmail($famID){
 	ini_set("sendmail_from", "vbs@hopecherryville.org");
 
 
-//@@	$mail_status = mail(SEND_TO, SUBJECT, $mailbody, $mail_header);
-	$mail_status = mail(VBS_EMAIL, 'VBS Registration', $mailbody, $mail_header);
+	$mail_status = mail("david@the-zieglers.com", SUBJECT, $mailbody, $mail_header);
+//@@TEST	$mail_status = mail(VBS_EMAIL, 'VBS Registration', $mailbody, $mail_header);
 	if(!$mail_status){
-         writeLog("[VBS MAIL] Mail could not be sent due to an error while trying to send the mail.  Mail status is $mail_status");
+         writeLog(FILE_NAME . __LINE__ . "  Mail could not be sent due to an error while trying to send the mail.  Mail status is $mail_status");
 	}
 
 	/* HLR lookup api - to get mobil carrier name */
 
 	if (SEND_TEXT){
-		writelog("Sending text message to " . VBS_TEXT);
+		writeLog(FILE_NAME . __LINE__ . " Sending text message to " . VBS_TEXT);
 		$text_headers = 'From: vbs@hopecherryville.org' . "\r\n";
 		$textMsg  = "New VBS registration for " . trim($family['family_name']) . " for $studentTotal students and $staffTotal volunteers\r\n";
-		/*
-		writelog("Sending to " . VBS_TEXT);
-		writelog("Sending from " . $text_headers);
-		writelog("Sending message " . $textMsg);
-		*/
 		$mail_status = mail(VBS_TEXT, '', $textMsg, $text_headers);
-		writelog("[VBS TEXT] Text mail status is " . $mail_status );
+		writeLog(FILE_NAME . __LINE__ . " Text mail status is " . $mail_status );
 	}
 
 }
@@ -116,17 +112,57 @@ function sendConfo($famID){
 
 	$email = getEmail($famID);
 
-	writelog("Sent to " . $email['family_name'] . " at " . $email['email']);
+	writeLog(FILE_NAME . __LINE__ . " Sent to " . $email['family_name'] . " at " . $email['email']);
 	$sendTo = $email['family_name'] . "<" . $email['email'] . ">";
 
 	$mail_status = mail($sendTo, "VBS Confirmation", $mailbody, implode("\r\n", $headers));
 	if(!$mail_status){
-         writeErr("Failed to send email to " . $sendTo . " on " . date("F dS, Y"));
+         writeErr(FILE_NAME . "Failed to send email to " . $sendTo . " on " . date("F dS, Y"));
 	}
 	else{
-		writeLog("Sent confirmation email to " . $email['family_name'] . " at " . $email['email']);
+		writeLog(FILE_NAME . __LINE__ . " Sent confirmation email to " . $email['family_name'] . " at " . $email['email']);
 	}
 }
+
+/* This function generates email to the clearance coordinator for follow up with the registrant(s) */
+function sendClearanceMail($famID){
+    
+    $mailbody = "<html><head>";
+    $mailbody .= getStyles();
+    $mailbody .= "</head><body>";
+    $mailbody .= "<h1>VBS Adult Volunteer Registration Confirmation.</h1><br />";
+    $mailbody .= "Registration date: " . date("F dS, Y") . "<br /><br />";
+    
+    $mailbody .="The following have registered as adult volunteers for VBS " . date("Y") . ". The following information has been sent to the VBS office.";
+    $mailbody .=" If any of the information is not correct, please <a href=\"mailto:vbs@hopecherryville.org\">email us</a> so we may adjust our ";
+    $mailbody .="records.<br><br>";
+    $mailbody .= formatFamily($famID) . "\r\n";
+    $mailbody .= formatStaff($famID) . "\r\n";
+    $mailbody .= "</body></html>";			/* just so we're well formed ! */
+    
+    // Append lines to $mail_header that you wish to be added to the headers of the e-mail. (SMTP Format
+    // with newline char ending each line)
+    $headers = array();
+    $headers[] = "MIME-Version: 1.0";
+    $headers[] = "From: VBS Registration Office <" . VBS_EMAIL . ">";
+    $headers[] = "Reply-to: VBS Registration Office <" . VBS_EMAIL . ">";
+    $headers[] = "Content-Type: text/html; charset=utf-8";
+    $headers[] = "Subject: VBS Confirmation";
+    
+    $email = getEmail($famID);
+    
+    writeLog(FILE_NAME . __LINE__ . " Sent to Clearance Coordinator at clearances@hopecherryville.org");
+    $sendTo = "Clearance Coordinator <" . CLEARANCES_EMAIL . ">";
+    
+    $mail_status = mail($sendTo, "VBS Adult Volunteer Registration", $mailbody, implode("\r\n", $headers));
+    if(!$mail_status){
+        writeErr(FILE_NAME . "Failed to send email to " . $sendTo . " on " . date("F dS, Y"));
+    }
+    else{
+        writeLog("Sent confirmation email to " . $email['family_name'] . " at " . $email['email']);
+    }
+}
+
 
 function formatFamily($famID){
 	global $vbsDBi, $family;
@@ -146,7 +182,6 @@ function formatFamily($famID){
 		$fam .= '<tr><td class="label">City State Zip:</td><td class="value">' . $family['city'] . " " . $family['state'] . " " . $family['zipcode'] . "</td></tr>";
 		$fam .= '<tr><td class="label">Email:</td><td class="value">'.$family['email']."</a></td></tr>";
 		$fam .= "<tr><td class='label'>Home Church:</td><td class='value'>" . $family['home_church'] . "</td></tr>";
-		$fam .= "<tr><td class='label'>Prep help:</td><td class='value'>" . (($family['prehelp']=="Y") ? "Yes" : "No") . "</td></tr>";
 		if (strlen(trim($family['comments']))>0){
 			$fam .= "<tr><td class='label'>Comments:</td><td>".$family["comments"]."</td></tr>";
 		}
@@ -165,22 +200,20 @@ function formatFamily($famID){
 function formatConfoPhone($famID){
 global $vbsDBi;
 
-	$sql = "SELECT contact_name, phone, phone_type_desc FROM phone_numbers JOIN phone_types ON phone_numbers.phone_type_code = phone_types.phone_type_code
-			WHERE family_id=" . $famID . " ORDER BY contact_name";
+	$sql = "SELECT contact_name, phone FROM phone_numbers WHERE family_id=" . $famID . " ORDER BY contact_name";
 	$result = mysqli_query($vbsDBi, $sql);
 	if ($result===false){
 		$sqlErr = mysqli_error($vbsDBi);
-		writeErr("No phone contacts for " . $famID, __FUNCTION__, __LINE__, $sqlErr);}
+		writeErr(FILE_NAME . "No phone contacts for " . $famID, __FUNCTION__, __LINE__, $sqlErr);}
 	else {
 		$phone = mysqli_fetch_assoc($result);
 		$ph = '<div id="Phone">';
 		$ph .= "<h2>Phone Contacts</h2>";
 		$ph .= '<table class="confo" cellspacing="0">';
-		$ph .= '<tr><th>Contact Name</th><th>Telephone Number</th><th>Phone Type</th></tr>';
+		$ph .= '<tr><th>Contact Name</th><th>Telephone Number</th></tr>';
 		do {
 			$ph .= "<tr><td>" . $phone['contact_name'] . "</td>";
-			$ph .= "<td>" . formatPhone($phone['phone']) . "</td>";
-			$ph .= "<td>" . $phone['phone_type_desc'] . "</td></tr>";
+			$ph .= "<td>" . formatPhone($phone['phone']) . "</td></tr>";
 		} while ($phone = mysqli_fetch_assoc($result));
 		$ph .= "</table></div>";
 	}
@@ -213,12 +246,12 @@ global $vbsDBi, $studentTotal;
 			$stud .= "<th>Class</th><th>T-Shirt</th><th>Friend Request</th><th>Comments</th></tr>";
 			$stud .= "<tr>";
 			do {
-				$stud .= "<td style='white-space: nowrap;'>" . $s['name'] . "</td>";
-				$stud .= "<td style='white-space: nowrap;'>" . $s['birthdate'] . "</td>";
-				$stud .= "<td>" . (($s['picture']=='Y')?"Yes":"No") . "</td>";
-				$stud .= "<td style='white-space: nowrap;'>" . $s['class'] . "</td>";
-				$stud .= "<td style='white-space: nowrap;'>" . $s['shirt_size'] . "</td>";
-				$stud .= "<td style='white-space: nowrap;'>" . $s['buddy'] . "</td>";
+				$stud .= "<td class='nowrap'>" . $s['name'] . "</td>";
+				$stud .= "<td class='nowrap centerText'>" . $s['birthdate'] . "</td>";
+				$stud .= "<td class='centerText'>" . (($s['picture']=='Y')?"Yes":"No") . "</td>";
+				$stud .= "<td class='centerText'>" . $s['class'] . "</td>";
+				$stud .= "<td class='centerText'>" . $s['shirt_size'] . "</td>";
+				$stud .= "<td class='centerText'>" . $s['buddy'] . "</td>";
 				$stud .= "<td>" . $s['comments'] . "</td>";
 				//20180402-removed confo no.$stud .= "<td>" . $s['confo'] . "</td>";
 				$stud .= "</tr>";
@@ -264,24 +297,24 @@ global $vbsDBi, $staffTotal;
 			$stf .= "<th>Picture</th>";
 			$stf .= "<th>T-Shirt</th><th>Teach with</th>";
 			//20180402-removed confo no. $stf .= "<th>Age Group</th><th>Conf #</th></tr>";
-			$stf .= "<th>Age Group</th></tr>";
+			$stf .= "<th>Clearance Required</th></tr>";
 
 			do {
 				$stf .= "<tr>";
-				$stf .= "<td >" . trim($s['name']) . "</td>";
-				$stf .= "<td>" . $s['mon'] . "</td>";
-				$stf .= "<td>" . $s['tue'] . "</td>";
-				$stf .= "<td>" . $s['wed'] . "</td>";
-				$stf .= "<td>" . $s['thur'] . "</td>";
-				$stf .= "<td>" . $s['fri'] . "</td>";
-				$stf .= "<td>" . $s['classroom'] . "</td>";
-				$stf .= "<td>" . $s['craft'] . "</td>";
-				$stf .= "<td>" . $s['kitchen'] . "</td>";
-				$stf .= "<td>" . $s['anything'] . "</td>";
-				$stf .= "<td>" . $s['picture'] . "</td>";
-				$stf .= "<td>" . $s['shirt_size'] . "</td>";
-				$stf .= "<td>" . $s['teach_with'] . "</td>";
-				$stf .= "<td>" . $s['age_group'] . "</td>";
+				$stf .= "<td>".trim($s['name'])."</td>";
+				$stf .= "<td class='centerText'>" . $s['mon'] . "</td>";
+				$stf .= "<td class='centerText'>" . $s['tue'] . "</td>";
+				$stf .= "<td class='centerText'>" . $s['wed'] . "</td>";
+				$stf .= "<td class='centerText'>" . $s['thur'] . "</td>";
+				$stf .= "<td class='centerText'>" . $s['fri'] . "</td>";
+				$stf .= "<td class='centerText'>" . $s['classroom'] . "</td>";
+				$stf .= "<td class='centerText'>" . $s['craft'] . "</td>";
+				$stf .= "<td class='centerText'>" . $s['kitchen'] . "</td>";
+				$stf .= "<td class='centerText'>" . $s['anything'] . "</td>";
+				$stf .= "<td class='centerText'>" . $s['picture'] . "</td>";
+				$stf .= "<td class='centerText'>" . $s['shirt_size'] . "</td>";
+				$stf .= "<td class='centerText'>" . $s['teach_with'] . "</td>";
+				$stf .= "<td class='centerText'>" . ($s['age_group']=='Adult'?'Y':'N') . "</td>";
 				//$stf .= "<td>" . $s['confo'] . "</td>";
 				$stf .= "</tr>";
 				$staffTotal++;
@@ -327,10 +360,10 @@ function getEmail($famID){
 function getFamilyErrors(){
 	global $vbsDBi;
 	$errMsg = "";
-	$notEmpty = Array('family_name'=>'Family Name', 'email'=>'Email', 'address'=>'Address', 'zipcode'=>'Zipcode', 'home_church'=>'Home Church', 'pre_help'=>'Pre-Help');
+	$notEmpty = Array('family_name'=>'Family Name', 'email'=>'Email', 'address'=>'Address', 'zipcode'=>'Zipcode', 'home_church'=>'Home Church');
 
 	/* Pull the family record */
-	$sql = "SELECT family_name, email, address, zipcode, home_church, prehelp from family where family_id=" . $_SESSION['family_id'];
+	$sql = "SELECT family_name, email, address, zipcode, home_church from family where family_id=" . $_SESSION['family_id'];
 	$rsResult = mysqli_query($vbsDBi, $sql);
 	if ($rsResult){
 		$rsFamily = mysqli_fetch_assoc($rsResult);
@@ -407,8 +440,7 @@ function getStaffErrors(){
 }
 
 /*******  PHONE RULES ************************************
-1. Must have at least one non-emergency number.
-2. Must have at least one emergency number.
+1. Must have at least two different numbers
 
 return: an unformatted string containing an error message.
 **********************************************************/
@@ -416,33 +448,37 @@ function getPhoneErrors(){
 	global $vbsDBi;
 	$errMsg = "";
 
-	$sqlNone = "SELECT count(*) as c from phone_numbers where phone_type_code <> 'E' AND family_id=" . $_SESSION['family_id'];
-	$sqlEmer = "SELECT count(*) as c from phone_numbers where phone_type_code = 'E' AND family_id=" . $_SESSION['family_id'];
+	$sqlPhoneCount = "SELECT count(distinct phone) as c from phone_numbers where family_id=" . $_SESSION['family_id'];
 
-	$result = $vbsDBi->query($sqlNone);
-	$nonEmergency = $result->fetch_object()->c;
-	$result = $vbsDBi->query($sqlEmer);
-	$emergency = $result->fetch_object()->c;
+	$result = $vbsDBi->query($sqlPhoneCount);
+	$phoneCount = $result->fetch_object()->c;
 	$result->free();
 
-	if ($emergency<1){
-		$errMsg = "At least one emergency contact number is required.";
+	if ($phoneCount<2){
+		$errMsg = "At least two phone contact numbers are required.";
 	}
-	if ($nonEmergency<1){
-		$errMsg .= "At least one non-emergency contact number is required.";
-	}
+	
 	return $errMsg;
 }
 
 /****************************************************************************
 	Returns the number of registered staff members for this current family id
+    NEEDING_CLEARANCES returns only count of volunteers over 18.
+    ALL_VOLUNEETERS returns count of all volunteers regardles of age.	
 *****************************************************************************/
-function getStaffCount(){
-	$sql = "SELECT count(*) as c from staff where registered = 'Y' and family_id = " . $_SESSION['family_id'];
+function getStaffCount($volunteerType){
+	
+    $sql = "SELECT count(*) from staff where registered = 'C' and family_id = " . $_SESSION['family_id'];
+    if ($volunteerType==ADULT_VOLUNTEERS) {
+        $sql .= " and age_group='Adult'";
+    }
 	$result = mysqli_query($vbsDBi, $sql);
 	$staffCount = mysqli_fetch_assoc($result);
+	if (DEBUG) print 'Staff Count:' . print_r($staffCount);
+	writeLog(FILE_NAME . __LINE__ . $staffCount[0] . ($volunteerType==ADULT_VOLUNTEERS ? " Adult volunteers" : " Volunteers"));
 	mysqli_free_result($result);
-	return $staffCount;
+	
+	return ($staffCount==0 ? FALSE : TRUE);
 }
 
 /***********************************************************************
@@ -455,6 +491,8 @@ function getStudentCount($famID){
 	mysqli_free_result($result);
 	return $registered[0];
 }
+
+/**************************************************** M A I N *********************/
 
 /*************************************************************************
  If the page was re-submitted to send an email, the REQUEST submit object
@@ -474,7 +512,13 @@ if (isset($_POST['submit'])) {
 				//@@sendConfo($_SESSION['family_id']);
 				//@@writeLog("Sent confirmation email to " . getEmail($_SESSION['family_id'])['family_name']);
 				sendVBSMail($_SESSION['family_id']);
-				header("Location: " . FINAL_PAGE);
+				if (getStaffCount(ADULT_VOLUNTEERS)>0){
+				    sendClearanceMail($famID);
+				    header("Location: " . CLEARANCE_PAGE);
+				}
+				else {
+				    header("Location: " . FINAL_PAGE);
+				}
 				break;
 			}
 			else
@@ -503,6 +547,8 @@ else
 	$err = getStudentErrors();
 	if (strlen(trim($err))>0) header("location: " . STUDENT_PAGE);
 	$err = "";
+}
+if (DEBUG) print "Total students = $studentTotal.  Total staff = $staffTotal.<br>";
 
 ?>
     <!doctype html>
@@ -528,17 +574,14 @@ else
         echo formatStudents($_SESSION['family_id']);
         echo formatStaff($_SESSION['family_id']);
     ?>
-    <p>&nbsp</p>
+    <p>&nbsp;</p>
     <div id="buttonGroup" class="center">
 		<form method="post" name="frmStudent" target="_self" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" style="display:inline-block">
 			<input type="submit" name="submit" class="button" value="<?php echo HOME_BUTTON?>">&nbsp;
-			<input type="submit" name="submit" class="button" value="<?php echo SUBMIT_REGISTRATION?>">&nbsp;&nbsp;&nbsp;
+			<input type="submit" name="submit" class="button" value="<?php echo SUBMIT_REGISTRATION;?>" <?php echo (($studentTotal+$staffTotal==0)?'disabled':'');?>>&nbsp;&nbsp;&nbsp;
 			<input type="submit" name="submit" class="button" value="<?php echo PREVIOUS_BUTTON?>">&nbsp;
 		</form>
     </div>
     </div>
     </body>
     </html>
-<?php
-}
-?>
