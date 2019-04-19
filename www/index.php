61db7128cd47f03aba66c8cf8bcd8672be982301
David Ziegler <drziegler@users.noreply.github.com>
2019-04-19 16:16:06 -0400

    Removed hard coded 0 counter
    Replaced with function to return registered students.

--- www/index.php
+++ www/index.php
@@ -32,7 +32,7 @@ mysqli_free_result($result);
 	<h2>VBS Information</h2>
 	<p>Welcome to the VBS information page for Hope Lutheran Church.  VBS starts on Monday, July 8 and runs through July 12th.  Daily hours are 9 AM to 12 PM with a closing program on Friday evening at 7 PM.  All students and staff must register to attend VBS.  On-line registration is easy. Registration questions?  Email the VBS office (<a href="mailto:vbs@hopecherryville.org" class="email">vbs@hopecherryville.org</a>) or call 610-767-7203 Ext 17.  Clearance questions?  Email the <a href="mailto:clearances@hopecherryville.org">Clearance Coordinator</a>.</p>
     <div id="logo"><a href="register.php"><img src="images/vbs-logo.jpg"></a></div>
-	<div id="counter">0&nbsp;students&nbsp;marooned on Mars</div>
+	<div id="counter"><?php echo registrationCount() ?>students&nbsp;marooned on Mars</div>
 	<div id="Footer">&copy;<?php echo date("Y") ?> David R. Ziegler &amp; Hope Lutheran Church</div>
 </div>
 </body>
