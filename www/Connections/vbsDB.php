f2f84f34879eafcc81511f4d02475bdb057c03ed
David Ziegler <drziegler@users.noreply.github.com>
2019-04-27 22:22:41 -0400

    Added environment support for dynamic config

--- www/Connections/vbsDB.php
+++ www/Connections/vbsDB.php
@@ -1,12 +1,18 @@
 <?php
-# FileName="Connection_php_mysql.htm"
-# Type="MYSQL"
-# HTTP="true"
-#$hostname_vbsDB = "localhost";
-$hostname_vbsDB = "dziegler3.dotstermysql.com";
-$database_vbsDB = "vbs2";
-$username_vbsDB = "phpvbsuser";
-$password_vbsDB = "vbs13";
-$vbsDB = mysql_pconnect($hostname_vbsDB, $username_vbsDB, $password_vbsDB) or trigger_error(mysql_error(),E_USER_ERROR);
+//define('ENV',	'PROD');
+define('ENV',	'DEV');
+
+if (ENV=='PROD') {
+	$hostname_vbsDB = "dziegler3.dotstermysql.com";
+	$database_vbsDB = "vbs2";
+	$username_vbsDB = "phpvbsuser";
+	$password_vbsDB = "vbs13";
+}
+else {
+	$hostname_vbsDB = "localhost";
+	$database_vbsDB = "vbs2";
+	$username_vbsDB = "phpvbsuser";
+	$password_vbsDB = "vbs13";
+}
 $vbsDBi = mysqli_connect($hostname_vbsDB, $username_vbsDB, $password_vbsDB, $database_vbsDB);
 ?>
