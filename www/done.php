2fbdfb8732bd6d835d4fdf98a620d69935bb7ffc
David Ziegler <drziegler@users.noreply.github.com>
2019-05-01 00:20:23 -0400

    removed unused stylesheets

--- www/done.php
+++ www/done.php
@@ -6,19 +6,15 @@ include('vbsUtils.inc');
 <head>
 <meta charset="utf-8">
 <title>Untitled Document</title>
-<link rel="stylesheet" media="screen and (min-width: 320px) and (max-width: 480px)" href="css/mobile.css" >
-<link rel="stylesheet" media="screen and (min-width: 768px) and (max-width: 1024px)" href="css/tablet.css" >
-<link rel="stylesheet" media="screen and (min-width: 1224px)" href="css/desktop.css" >
-
+<link href="css/vbs.css" rel="stylesheet" type="text/css">
+<link href="css/layout.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-<div class="gridContainer">
 	<div class="center"><h1>THANKS FOR REGISTERING</h1></div>
 	<div id="buttonSubGroup" style="position:fixed; width:100%; top:0;">
 		<a href="<?php echo HOME_PAGE?>"><input type="submit" class="button" name="submit" value="<?php echo HOME_BUTTON?>"></a>
 	</div>
-	<div id="done-logo" class="vertical-center"><center><img src="images/vbs-logo.jpg"></center></div>
-	<div style="position:fixed; width:99%; bottom:0"><h1 class="center">See you in July</h1></div>
-</div>
+	<center><img src="images/vbs-logo.jpg"></center>
+	<div style="position:fixed; width:100%; bottom:0"><h1>See you in July</h1></div>
 </body>
 </html>
