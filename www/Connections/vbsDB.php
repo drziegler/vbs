5710b8999b87065bc70f31602d2449e08c441289
David Ziegler <drziegler@users.noreply.github.com>
2020-02-03 19:33:21 -0500

    Update vbsDB.php

--- www/Connections/vbsDB.php
+++ www/Connections/vbsDB.php
@@ -1,18 +1,28 @@
 <?php
 //define('ENV',	'PROD');
+//define('ENV', 'TEST');
 define('ENV',	'DEV');
 
-if (ENV=='PROD') {
-	$hostname_vbsDB = "dziegler3.dotstermysql.com";
-	$database_vbsDB = "vbs2";
-	$username_vbsDB = "phpvbsuser";
-	$password_vbsDB = "vbs13";
-}
-else {
-	$hostname_vbsDB = "localhost";
-	$database_vbsDB = "vbs2";
-	$username_vbsDB = "phpvbsuser";
-	$password_vbsDB = "vbs13";
+switch(ENV){
+    case 'PROD' :
+        $hostname_vbsDB = "dziegler3.dotstermysql.com";
+        $database_vbsDB = "vbs2";
+        $username_vbsDB = "phpvbsuser";
+        $password_vbsDB = "vbs13";
+        break;
+    case 'TEST' :
+        $hostname_vbsDB = "dziegler3.dotstermysql.com";
+        $database_vbsDB = "vbs2test";
+        $username_vbsDB = "phpvbsusertest";
+        $password_vbsDB = "vbs19";
+        break;
+        
+    case 'DEV' :
+        $hostname_vbsDB = "localhost";
+        $database_vbsDB = "vbs2";
+        $username_vbsDB = "phpvbsuser";
+        $password_vbsDB = "vbs13";
+        break;
 }
 $vbsDBi = mysqli_connect($hostname_vbsDB, $username_vbsDB, $password_vbsDB, $database_vbsDB);
 ?>
