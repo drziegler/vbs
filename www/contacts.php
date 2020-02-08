6c55a5c15d702bf491cd97ff761a95194cdefce7
David Ziegler <drziegler@users.noreply.github.com>
2020-02-07 23:40:58 -0500

    Removed old browser references

--- www/contacts.php
+++ www/contacts.php
@@ -14,7 +14,7 @@ if ($_SESSION['family_id']==0){
 
 function quickSave(){
     global $vbsDBi;
-    if (DEBUG) print "Line " . __LINE__ . " Quick Save: $_POST = " . print_r($_POST, TRUE) . '<br>';
+    if (DEBUG) print "Line " . __LINE__ . ' Quick Save: $_POST = ' . print_r($_POST) . '<br>';
     writeLog(FILE_NAME . __LINE__ . " Entering quickSave()");
 
     $sql = "DELETE from phone_numbers WHERE family_id=" . $_SESSION['family_id'];
@@ -278,8 +278,10 @@ switch ($_POST['submit']){
 			if (($_POST['submit']=='Add') or (count($rsPhone)<1)){
 				if (DEBUG) print "Line: " . __LINE__ . "<br>";
 				/* Put a blank record on the end of the array and redisplay */
+				if (DEBUG) print 'Before: '; print_r($rsPhone); print '<br>';
 				$rsTemp = $rsPhone;
 				$rsPhone = array_merge($rsTemp, $blankPhoneArray);
+				if (DEBUG) print 'After: '; print_r($rsPhone); print '<br>';
 			}
 		}
 		else {
@@ -306,8 +308,10 @@ if ($rsResult){
     if (($_POST['submit']=='Add') or (count($rsPhone)<1)){
         if (DEBUG) print "Line: " . __LINE__ . "<br>";
         /* Put a blank record on the end of the array and redisplay */
+        if (DEBUG) print "Before: "; print_r($rsPhone); print "<br>";
         $rsTemp = $rsPhone;
         $rsPhone = array_merge($rsTemp, $blankPhoneArray);
+        if (DEBUG) print "After: "; print_r($rsPhone); print '<br>';
     }
 }
 else {
@@ -323,60 +327,53 @@ if (DEBUG) {
 ?>
 
 <!doctype html>
-<!--[if lt IE 7]> <html class="ie6 oldie"> <![endif]-->
-<!--[if IE 7]>    <html class="ie7 oldie"> <![endif]-->
-<!--[if IE 8]>    <html class="ie8 oldie"> <![endif]-->
-<!--[if gt IE 8]><!-->
 <html class="">
-<!--<![endif]-->
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>VBS-Contact Information</title>
-<!-- <link href="css/boilerplate.css" rel="stylesheet" type="text/css"> -->
 <link href="css/layout.css" rel="stylesheet" type="text/css">
-<!--[if lt IE 9]>
-<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-<![endif]-->
 <script src="respond.min.js"></script>
 <script src="scripts/vbsUtils.js"></script>
 </head>
 <title>VBS-Telephone Contacts</title>
 <body>
 <div id="Find" class="gridContainer">
-	<h1>Contact Info</h1>
-	<div id="dataLayout">
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>" method="POST" name="frmContacts" target="_self">
-<table id="Phone" border="1">
-    <?php if (strlen($errMsgText)>0) { ?>
-    	<tr><td colspan="3" class="center error"><?php echo $errMsgText;?></td></tr>
-    <?php } else { ?>
-    	<tr><td colspan="3" class="center">Provide at least two different phone numbers.</td></tr>
-    <?php } ?>
-	<tr><th>*&nbsp;Name</th><th>*&nbsp;Phone</th><th>Select</th></tr>
-    <?php for ($i=0; $i<count($rsPhone); $i++){ ?>
-    <tr>
-        <td><input type="text" name="phone[<?php echo $i;?>][contact_name]" value="<?php echo $rsPhone[$i]['contact_name']; ?>" style="width:99%" maxlength="50"></td>
-        <td class="col2"><input type="text" name="phone[<?php echo $i;?>][phone]" maxlength="12" value="<?php echo formatPhone($rsPhone[$i]['phone']); ?>" style="width:99%"></td>
-		<td class="center col3"><input name="phone[<?php echo $i;?>][sel]" type="checkbox" value="">
-        <input type="hidden" name="phone[<?php echo $i;?>][family_id]" value="<?php echo $_SESSION['family_id']?>"></td>
-	</tr>
-    <?php } ?>
-    <tr><td colspan="3"><span class="float-left">*&nbsp;required</span>
-    	<span class="horizontal-center">
-    	<input type="submit" name="submit" value="Save">&nbsp;
-	    <input type="submit" name="submit" value="Add">&nbsp;
-	    <input type="submit" name="submit" value="Delete">&nbsp;
-		</span>
-		<span class="popup float-right" onclick="myPopUp('help')">Help available<span class="popuptext" id="help">Enter family contact information on this page.  You may enter as many names and phone numbers as you wish but you must provide at least two different phone numbers.  Each contact must have a name &amp; phone number.<br>To delete a contact, first check the Select box(es) of the lines you want to delete then click the delete button.</span></span>
-	</td></tr>	
-</table>
-<div id="buttonGroup" class="center">
-	<input type="submit" name="submit" class="button" value="<?php echo HOME_BUTTON?>">&nbsp;
-    <input type="submit" name="submit" class="button" value="<?php echo PREVIOUS_BUTTON?>">&nbsp;
-    <input type="submit" name="submit" class="button" value="<?php echo NEXT_PAGE?>">
-</div>
-</form>
+<h1>Contact Info</h1>
+<div id="dataLayout">
+	<div id="Contacts">
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"])?>" method="POST" name="frmContacts" target="_self">
+    <table border="1">
+        <?php if (strlen($errMsgText)>0) { ?>
+        	<tr><td colspan="3" class="center title nowrap error"><?php echo $errMsgText;?></td></tr>
+        <?php } else { ?>
+        	<tr><td colspan="3" class="center title nowrap">Provide at least two different phone numbers.</td></tr>
+        <?php } ?>
+    	<tr><th>*&nbsp;Name</th><th>*&nbsp;Phone</th><th>Select</th></tr>
+        <?php for ($i=0; $i<count($rsPhone); $i++){ ?>
+        <tr>
+            <td class="center col1"><input type="text" name="phone[<?php echo $i;?>][contact_name]" value="<?php echo $rsPhone[$i]['contact_name']; ?>" maxlength="50"></td>
+            <td class="center col2"><input type="text" name="phone[<?php echo $i;?>][phone]" maxlength="12" value="<?php echo formatPhone($rsPhone[$i]['phone']); ?>" ></td>
+    		<td class="center col3"><input name="phone[<?php echo $i;?>][sel]" type="checkbox" value="">
+            <input type="hidden" name="phone[<?php echo $i;?>][family_id]" value="<?php echo $_SESSION['family_id']?>"></td>
+    	</tr>
+        <?php } ?>
+        <tr><td colspan="3"><span class="float-left">*&nbsp;required</span>
+        	<span class="horizontal-center">
+        	<input type="submit" name="submit" value="Save">&nbsp;
+    	    <input type="submit" name="submit" value="Add">&nbsp;
+    	    <input type="submit" name="submit" value="Delete">&nbsp;
+    		</span>
+    		<span class="popup float-right" onclick="myPopUp('help')">Help available<span class="popuptext" id="help">Enter family contact information on this page.  You may enter as many names and phone numbers as you wish but you must provide at least two different phone numbers.  Each contact must have a name &amp; phone number.<br>To delete a contact, first check the Select box(es) of the lines you want to delete then click the delete button.</span></span>
+    	</td></tr>	
+    </table>
+    <div id="buttonGroup" class="center">
+    	<input type="submit" name="submit" class="button" value="<?php echo HOME_BUTTON?>">&nbsp;
+        <input type="submit" name="submit" class="button" value="<?php echo PREVIOUS_BUTTON?>">&nbsp;
+        <input type="submit" name="submit" class="button" value="<?php echo NEXT_PAGE?>">
+    </div>
+    </form>
+	</div>
 </div>
 </div>
 </body>
