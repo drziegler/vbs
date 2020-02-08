b474165906f4c0e771f10225b5e14c04ae9819d1
David Ziegler <drziegler@users.noreply.github.com>
2020-02-07 23:13:23 -0500

    Removed obsolete references
    Obsolete references to confoNo, boilerplate.css

--- www/staff.php
+++ www/staff.php
@@ -487,21 +487,12 @@ $offset = --$offset;
 
 ?>
 <!doctype html>
-<!--[if lt IE 7]> <html class="ie6 oldie"> <![endif]-->
-<!--[if IE 7]>    <html class="ie7 oldie"> <![endif]-->
-<!--[if IE 8]>    <html class="ie8 oldie"> <![endif]-->
-<!--[if gt IE 8]><!-->
 <html class="">
-<!--<![endif]-->
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>VBS Staff</title>
-<!-- <link href="css/boilerplate.css" rel="stylesheet" type="text/css"> -->
 <link href="css/layout.css?v7" rel="stylesheet" type="text/css">
-<!--[if lt IE 9]>
-<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-<![endif]-->
 <script src="scripts/vbsUtils.js"></script>
 </head>
 <body>
