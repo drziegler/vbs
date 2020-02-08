e682a8dff0c324923ecfe31fccd2ed03aef3b069
David Ziegler <drziegler@users.noreply.github.com>
2020-02-07 23:42:48 -0500

    Centered image on page

--- www/done.php
+++ www/done.php
@@ -9,11 +9,11 @@ include('vbsUtils.inc');
 <link href="css/layout.css" rel="stylesheet" type="text/css">
 </head>
 <body>
-	<div id="Find" class="center"><h1>THANKS FOR REGISTERING</h1></div>
+	<div id="Find" class="center"><h1>Thanks for registering</h1></div>
 	<div id="buttonSubGroup" style="position:fixed; width:100%; top:0;">
 		<a href="<?php echo HOME_PAGE?>"><input type="submit" class="button" name="submit" value="<?php echo HOME_BUTTON?>"></a>
 	</div>
-	<center><img src="images/vbs-logo.jpg"></center>
+	<div class="vertical-horizontal-center"><img src="images/vbs-logo.jpg"></div>
 	<div style="position:fixed; width:100%; bottom:0"><h1>See you in July</h1></div>
 </body>
 </html>
