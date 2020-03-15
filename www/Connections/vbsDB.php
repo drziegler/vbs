8628d0531ef1e0ed0443066090a043b29f9d7067
David Ziegler <drziegler@users.noreply.github.com>
2020-03-15 16:53:36 -0400

    Updated database name and password

--- www/Connections/vbsDB.php
+++ www/Connections/vbsDB.php
@@ -1,19 +1,19 @@
 <?php
-//define('ENV',	'PROD');
+define('ENV',	'PROD');
 //define('ENV', 'TEST');
-define('ENV',	'DEV');
+//define('ENV',	'DEV');
 
 switch(ENV){
     case 'PROD' :
         $hostname_vbsDB = "dziegler3.dotstermysql.com";
         $database_vbsDB = "vbs";
         $username_vbsDB = "phpvbsuser";
-        $password_vbsDB = "vbs20";
+        $password_vbsDB = "vbs19";
         break;
     case 'TEST' :
         $hostname_vbsDB = "dziegler3.dotstermysql.com";
         $database_vbsDB = "vbs2";
-        $username_vbsDB = "phpvbsuser";
+        $username_vbsDB = "phpvbsusertest";
         $password_vbsDB = "vbs19";
         break;
         
