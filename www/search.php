32d5780d61fe769d21421091c99e057b868db3e1
David Ziegler <drziegler@users.noreply.github.com>
2020-03-15 21:21:41 -0400

    Removed extraneous code, added footer

--- www/search.php
+++ www/search.php
@@ -16,29 +16,23 @@ switch ($_POST['submit']) {
 }
 ?>
 <!doctype html>
-<!--[if lt IE 7]> <html class="ie6 oldie"> <![endif]-->
-<!--[if IE 7]>    <html class="ie7 oldie"> <![endif]-->
-<!--[if IE 8]>    <html class="ie8 oldie"> <![endif]-->
-<!--[if gt IE 8]><!-->
 <html class="">
-<!--<![endif]-->
 <head>
 <title>VBS Search Page</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>VBS-Find Family</title>
 <link href="css/layout.css?v2" rel="stylesheet" type="text/css">
-<!--  @@ <script src="scripts/respond.min.js"></script> -->
 </head>
 <body>
-<div id="Find" class="gridContainer clearfix">
+<div id="Find" class="gridContainer-footer clearfix">
     <h1>Search</h1>
     <div id="dataLayout" class="horizontal-center">
     <div id="Search">
     <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post" name="frmPhone" target="_self">
         <table>
             <tr><td class="center">Enter your telephone number.</td></tr>
-    		<tr><td class="center"><input name="txtPhone" id="searchPhone" type="number" pattern="[1-9]" autofocus></td></tr>
+    		<tr><td class="center"><input name="txtPhone" id="searchPhone" type="number" pattern="\d{10}" maxlength="10" autofocus></td></tr>
         </table>
         <div id="buttonGroup" class="center">
     		<input type="submit" name="submit" class="button" value="<?php echo SEARCH ?>">&nbsp;
