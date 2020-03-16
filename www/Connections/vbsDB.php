c415d46f5519559fc768ecf342758014b4bd9293
David Ziegler <drziegler@users.noreply.github.com>
2020-03-15 21:13:26 -0400

    Enhancement to auto detect and set environment from file.

--- www/Connections/vbsDB.php
+++ www/Connections/vbsDB.php
@@ -1,23 +1,26 @@
 <?php
-define('ENV',	'PROD');
+/* WAS 3-15-20 */
+//define('ENV',	'PROD');
 //define('ENV', 'TEST');
 //define('ENV',	'DEV');
+/* NEW 3-15-20 */
+include_once('vbsUtils.inc');
 
-switch(ENV){
-    case 'PROD' :
+switch(VBS_ENV){
+    case VBS_ENV_PROD :
         $hostname_vbsDB = "dziegler3.dotstermysql.com";
         $database_vbsDB = "vbs";
         $username_vbsDB = "phpvbsuser";
         $password_vbsDB = "vbs19";
         break;
-    case 'TEST' :
+    case VBS_ENV_TEST :
         $hostname_vbsDB = "dziegler3.dotstermysql.com";
         $database_vbsDB = "vbs2";
-        $username_vbsDB = "phpvbsusertest";
+        $username_vbsDB = "phpvbsuser";
         $password_vbsDB = "vbs19";
         break;
         
-    case 'DEV' :
+    case VBS_ENV_DEV :
         $hostname_vbsDB = "localhost";
         $database_vbsDB = "vbs2";
         $username_vbsDB = "phpvbsuser";
